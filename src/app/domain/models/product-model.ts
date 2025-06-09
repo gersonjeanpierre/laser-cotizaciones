e0c060149ecbd14486859fdb93e968b8ab44ae9c
JalukOne <jalukone@outlook.com>
2025-06-09 11:03:41 -0500

    Add product model

---
+++ src/app/domain/models/product-model.ts
@@ -0,0 +1,35 @@
+import { FormControl } from "@angular/forms";
+
+export interface Product{
+  id: string;
+  name: string;
+  type: string;
+  unityMeasure: string; // metro lineal, metro cuadrado, item, etc.
+  baseCost: number;
+  imageUrl: string | null; // URL de la imagen del producto  
+  created_at: string | null;
+  updated_at: string | null; 
+}
+
+export interface ProductCreate {
+  name: string;
+  type: string;
+  unityMeasure: string; // metro lineal, metro cuadrado, item, etc.
+  baseCost: number;
+}
+
+export type ProductFormGroup = {
+  name: FormControl<string | null>;
+  type: FormControl<string | null>;
+  unityMeasure: FormControl<string | null>;
+  baseCost: FormControl<number | null>;
+};
+
+export interface ProductExtraOption {
+  id: string;
+  productoId: string;
+  description: string;
+  baseCost: number;
+  created_at: string | null;
+  updated_at: string | null;
+}
